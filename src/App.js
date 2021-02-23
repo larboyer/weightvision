094a01bf7ff948799bf6c779c942c8cefb660046
Larry Boyer <larry.boyer@gmail.com>
2021-02-22 20:58:17 -0500

    Added crude date picker and weight entry and a graph. Still rough.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { LineChart, Line } from 'recharts';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import NumericInput from 'react-numeric-input';
 
-function App() {
+let data = [
+	{name: 'Page A', uv: 400, pv: 2400, amt: 1422},
+	{name: 'Page B', uv: 230, pv: 1300, amt: 1877},
+	{name: 'Page D', uv: 400, pv: 1455, amt: 722},
+	{name: 'Page C', uv: 523, pv: 2435, amt: 2234},
+	{name: 'Page D', uv: 400, pv: 1455, amt: 722}
+];
+
+/*
+function doIt() {
+	console.log("test 001");
+}
+<DatePicker selected={startDate} onChange={doIt()} />
+*/
+
+const App = () => {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Weight Vision
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+		Pick Date
+        <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+		Pick Weight
+		<NumericInput min={0} precision={1} />
+		<LineChart width={400} height={400} data={data}>
+			<Line type="monotone" dataKey="uv" stroke="#8884d8" />
+			<Line type="monotone" dataKey="pv" stroke="#288448" />
+		</LineChart>
       </header>
     </div>
   );
-}
+};
+
 
 export default App;
